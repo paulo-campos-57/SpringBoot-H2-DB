b478f1c92f581529a87b56335961946d2e5964c4
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-03-07 20:03:45 -0300

    Create Cliente.java

---
+++ cliente/models/Cliente.java
@@ -0,0 +1,12 @@
+package com.cliente.cliente.models;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+
+	private long id;
+	private String nome;
+	private int idade;
+	private String sexo;
+}
